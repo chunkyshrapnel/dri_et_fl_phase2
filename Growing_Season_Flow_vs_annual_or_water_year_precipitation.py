bab39d3234ee00f429461b12c8ede1247d5f8b4c
chunkyshrapnel <60608146+chunkyshrapnel@users.noreply.github.com>
2022-07-19 17:15:02 -0600

    Add files via upload

--- Growing_Season_Flow_vs_annual_or_water_year_precipitation.py
+++ Growing_Season_Flow_vs_annual_or_water_year_precipitation.py
@@ -24,7 +24,7 @@ list_site = ['09180000',  # DOLORES RIVER NEAR CISCO, UT,
 # This dict is used to transform the var names of the columns into
 # more readable names to be used in the plot displays.
 dict_var_to_string_conversion = {
-    'mean_growing_season_flow': 'Mean Growing Season Flow (cfs)',
+    'mean_gs_flow': 'Mean Growing Season Flow (cfs)',
     'ann_pr': 'Annual Precipitation (PLACE HOLDER VAR)',
     'wy_pr': 'Water Year Precipitation (PLACE HOLDER VAR)',
     'gs_pr': 'Growing Season Precipitation (PLACE HOLDER VAR)'
@@ -58,7 +58,7 @@ def load_raw_data_and_join():
 
 # Takes in 2 strings that correspond to the 2 variables that will be charted on
 # the x and y axes of the scatter plot. This function will create a 6 x 2 grid of
-# plots where weach plot corresponds to a site.
+# plots where each plot corresponds to a site.
 def create_site_plots(fl_var, pr_var):
 
     # Data is loaded in
@@ -71,6 +71,7 @@ def create_site_plots(fl_var, pr_var):
     # A plot is created for each site and then appended onto 'list_of_monthly_figs'
     list_of_monthly_figs = []
     for site in list_site:
+
         site_name = df_metadata.loc[df_metadata['station_id'] == int(site), 'site_name'].iloc[0]
         df_station = df_data[df_data["site_name"] == site_name]
 
@@ -124,7 +125,8 @@ def create_site_plots(fl_var, pr_var):
         list_of_monthly_figs.append(p_site)
 
     # If a 'plots' directory does not exist, make it.
-    if not (os.path.isdir('plots')):
+    path = os.getcwd() + '/plots'
+    if not (os.path.isdir(path)):
         os.mkdir('plots')
 
     # Save the 6 plots to a 3 x 2 grid.
@@ -136,14 +138,90 @@ def create_site_plots(fl_var, pr_var):
 
 
 # Creates a massive scatter plot that contains all the sites.
-# Each growing season value is normalized by dividing 
-def create_normalized_combined_plot():
+# Each growing season value is normalized by dividing
+def create_normalized_combined_plot(fl_var, pr_var):
+    df_metadata = load_metadata()
+    df_data = load_raw_data_and_join()
+    output_file('normalized_' + fl_var + '_vs_' + pr_var + '.html')
 
+    list_of_dfs = []
+    for site in list_site:
+
+        # Break data down by Site
+        site_name = df_metadata.loc[df_metadata['station_id'] == int(site), 'site_name'].iloc[0]
+        df_station = df_data[df_data["site_name"] == site_name]
+
+        # Normalize the Data
+        mean = df_station['mean_gs_flow'].mean()
+        df_station = df_station.assign(discharge_mean_cfs=lambda x: (x['mean_gs_flow'] / mean))
+        list_of_dfs.append(df_station)
+    df = pd.concat(list_of_dfs)
+
+    p = figure(width=900, height=900)
+    p.xgrid.grid_line_color = None
+    p.ygrid.grid_line_color = None
+    circle = p.circle(x=pr_var, y=fl_var,
+                      source=ColumnDataSource(df),
+                      color='black', fill_color="#add8e6",
+                      size=8)
+
+    p.title.text = site_name + '\n' + dict_var_to_string_conversion[fl_var] + ' vs ' \
+                        + dict_var_to_string_conversion[pr_var]
+    p.title.text_font_size = '15pt'
+    p.yaxis.axis_label = dict_var_to_string_conversion[fl_var]
+    p.xaxis.axis_label = dict_var_to_string_conversion[pr_var]
+
+    # Calculate the least-square regression line
+    regression_line = np.polyfit(df[pr_var], df[fl_var], 1, full=True)
+    slope = regression_line[0][0]
+    intercept = regression_line[0][1]
+    y_predicted = [slope * i + intercept for i in df[pr_var]]
+    p.line(df[pr_var], y_predicted, color='black')
+
+    # Calculations to be used in the stats label on every scatter plot
+    pearson_r, pearson_p = stats.pearsonr(df[fl_var], df[pr_var])
+    kendall_r, kendall_p = stats.kendalltau(df[fl_var], df[pr_var])
+
+    # The stats label to be added.
+    label_text = 'Slope: ' + str(round(slope, 3)) + '\n' + \
+                 'Intercept: ' + str(round(intercept, 3)) + '\n' + \
+                 'Pearson r: ' + str(round(pearson_r, 3)) + '\n' + \
+                 'Pearson P-Value: ' + str(round(pearson_p, 3)) + '\n' + \
+                 'Kendall Tau: ' + str(round(kendall_r, 3)) + '\n' + \
+                 'Kendall P-Value: ' + str(round(kendall_p, 3)) + '\n' + \
+                 'n: ' + str(len(df))
+    label = Label(x=700, y=20, x_units='screen', y_units='screen',
+                  text_font_size='8pt', text=label_text)
+    p.add_layout(label)
+
+    hover = HoverTool()
+    hover.renderers = [circle]
+    hover.tooltips = [
+        ('Year', '@year'),
+        ('Site', '@site_name'),
+        (dict_var_to_string_conversion[fl_var], '@' + fl_var),
+        (dict_var_to_string_conversion[pr_var], '@' + pr_var)
+    ]
+    p.add_tools(hover)
+
+    # If a 'plots' directory does not exist, make it.
+    path = os.getcwd() + '/plots'
+    if not (os.path.isdir(path)):
+        os.mkdir('plots')
+
+    os.chdir('plots')
+    save(p)
+    os.chdir('..')
 
 
 def main():
-    create_site_plots('mean_growing_season_flow', 'ann_pr')
-    create_site_plots('mean_growing_season_flow', 'wy_pr')
-    create_site_plots('mean_growing_season_flow', 'gs_pr')
+
+    create_site_plots('mean_gs_flow', 'ann_pr')
+    create_site_plots('mean_gs_flow', 'wy_pr')
+    create_site_plots('mean_gs_flow', 'gs_pr')
+
+    create_normalized_combined_plot('mean_gs_flow', 'ann_pr')
+    create_normalized_combined_plot('mean_gs_flow', 'wy_pr')
+    create_normalized_combined_plot('mean_gs_flow', 'gs_pr')
 
 main()
